2615c2ecfa3fe232c1286398a3f5fcd6b361161a
Chsfdjr <114878424+Chsfdjr@users.noreply.github.com>
2024-04-29 10:37:18 +0200

    add first part

---
+++ first-part/script.js
@@ -0,0 +1,14 @@
+const exo1 = () => {
+    //Afficher en console "Hello World"
+}
+
+function exo2() {
+    //try... catch
+
+    //l'objectif est de diviser deux nombres
+    // à l'aide des règles mathématiques si la division n'est pas possible ecrire dans la sortie d'erreur "FAUX"
+    // dans la console " le résultat est : ${result}"
+}
+
+//exo 3
+// Afficher dans la console dans un object le nom et le mot de passe une fois le bouton submit pressé;
